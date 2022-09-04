5f41392d8bf58e5d1837735ff5ee48120b657ac7
Maria <rjdnansd@gmail.com>
2022-09-04 14:53:20 +0300

    Initial Commit

---
+++ Pharmacy/PharmacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmacyApp.swift
+//  Pharmacy
+//
+//  Created by Marichka on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PharmacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
